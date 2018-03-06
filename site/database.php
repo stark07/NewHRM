059b712368bf7f960fe072744eb4343eee52fbf0
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-06 19:34:45 +0000

    Update database.php

--- site/database.php
+++ site/database.php
@@ -1,5 +1,5 @@
 <?php
-include '../templates/header.php';
+include '/templates/header.php';
 ?>
 
 <!DOCTYPE html>
@@ -123,5 +123,5 @@ include '../templates/header.php';
 </html>
 
 
-<?php include '../templates/footer.php';
+<?php include '/templates/footer.php';
      ?>
