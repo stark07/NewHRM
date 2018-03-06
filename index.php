a384f7428258291f400dcb2b3b61af8708ed365d
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-06 16:43:22 +0000

    Update index.php

--- index.php
+++ index.php
@@ -21,15 +21,7 @@ include 'templates/header.php';
                           <?php echo ASLang::get('my_profile'); ?>
                       </a>
                   </li>
-                        
-                  <li>
-                      <a href="/site/protected_page.php">
-                          <i class="icon-user glyphicon glyphicon-user"></i>
-                          <i class="icon-chevron-right glyphicon glyphicon-chevron-right"></i> 
-                          <?php echo ASLang::get('my_database'); ?>
-                      </a>
-                  </li>
-                        
+           
                   <?php if($user->isAdmin()): ?>
                   <li>
                       <a href="users.php">
@@ -48,48 +40,6 @@ include 'templates/header.php';
                   <?php endif; ?>
                 </ul>
               </div>
-              <div class="span9">
-                    
-                    <div class="comments">
-                            <h3 id="comments-title">
-                              <?php echo ASLang::get('comments_wall'); ?> 
-                              <small><?php echo ASLang::get('last_7_posts'); ?></small>
-                            </h3>
-                            <div class="comments-comments">
-                                <?php $ASComment = new ASComment(); ?>
-                                <?php $comments = $ASComment->getComments(); ?>
-                                <?php foreach($comments as $comment): ?>
-                                 <blockquote>
-                                    <p><?php echo htmlentities( stripslashes($comment['comment']) ); ?></p>
-                                    <small>
-                                        <?php echo htmlentities($comment['posted_by_name']);  ?> 
-                                        <em> <?php echo ASLang::get('at'); ?> <?php echo $comment['post_time']; ?></em></small>
-                                </blockquote>
-                                <?php endforeach; ?>
-                            </div>
-                    </div>
-                
-                    <?php if($user->getRole() != 'user'): ?>
-                    <div class="leave-comment">
-                        <div class="control-group form-group">
-                            <h5><?php echo ASLang::get('leave_comment'); ?></h5>
-                            <div class="controls">
-                                <textarea class="form-control" id="comment-text"></textarea>
-                            </div>
-                        </div>
-                        <div class="control-group form-group">
-                             <div class="controls">
-                                <button class="btn btn-success" id="comment">
-                                  <?php echo ASLang::get('comment'); ?>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <?php else: ?>
-                        <p><?php echo ASLang::get('you_cant_post'); ?></p>
-                    <?php endif; ?>
-                    
-        
               </div>
             </div>
         
@@ -99,7 +49,5 @@ include 'templates/header.php';
 
     <script src="ASLibrary/js/asengine.js" type="text/javascript" charset="utf-8"></script>
     <script type="text/javascript" src="ASLibrary/js/index.js" charset="utf-8"></script>
-
-    
   </body>
 </html>
