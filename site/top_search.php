3410191253fecf0511d4c8626899c2d6e83191ef
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-10 22:10:05 +0000

    Create top_search.php

---
+++ site/top_search.php
@@ -0,0 +1,20 @@
+require_once 'config.php';
+
+	// Display 5 most recent search items
+	$query = "SELECT * FROM employee c INNER JOIN query_data q ON c.name = q.name ORDER BY querycount DESC LIMIT 5";
+	
+	// The search
+	$result = $test_db->query($query);
+	while($results = $result->fetch_array()) {
+		$result_array[] = $results;
+	}
+	
+	foreach ($result_array as $result) {
+		// The output
+		echo '<tr>';			
+		echo '<td class="small">'.$result['ID'].'</td>';
+		echo '<td class="small">'.$result['name'].'</td>';
+		echo '<td class="small">'.$result['address'].'</td>';
+		echo '<td class="small">'.$result['salary'].'</td>';
+		echo '</tr>';	
+	}
