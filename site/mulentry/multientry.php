f66009c4c729fa56cd2b9497514a268361415d85
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-10 22:42:49 +0000

    Update multientry.php

--- site/mulentry/multientry.php
+++ site/mulentry/multientry.php
@@ -6,13 +6,49 @@
     <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="//netsh.pp.ua/upwork-demo/1/js/typeahead.js"></script>
-    
+  <style>
+        h1 {
+            font-size: 20px;
+            color: #111;
+        }
+
+        .content {
+            width: 80%;
+            margin: 0 auto;
+            margin-top: 50px;
+        }
+
+        .tt-hint,
+        .city {
+            border: 2px solid #CCCCCC;
+            border-radius: 8px 8px 8px 8px;
+            font-size: 24px;
+            height: 45px;
+            line-height: 30px;
+            outline: medium none;
+            padding: 8px 12px;
+            width: 400px;
+        }
+
+        .tt-dropdown-menu {
+            width: 400px;
+            margin-top: 5px;
+            padding: 8px 12px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            border-radius: 8px 8px 8px 8px;
+            font-size: 18px;
+            color: #111;
+            background-color: #F1F1F1;
+        }
+    </style>  
     
   <script>
         $(document).ready(function() {
             $('input.name').typeahead({
                 name: 'name',
-                remote: 'mysql.php?query=%QUERY'
+                remote: 'name.php?query=%QUERY'
             });
         })
     </script>
@@ -24,9 +60,10 @@
 
         <form>
             <h6>Search Database</h6>
-            <input type="text" name="name" size="30" class="mysql" placeholder="Please Enter Name">
+            <input type="text" name="name" size="30" class="name" placeholder="Please Enter Name">
         </form>
     </div>
+  
   <div class="btn-group">
   <button type="button" class="btn btn-danger">Menu</button>
   <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -38,7 +75,7 @@
     <li><a href="../../profile.php">Profile</a></li>
     <li><a href="../../logout.php">Logout</a></li>
     <li role="separator" class="divider"></li>
-    <li><a href="#">Separated link</a></li>
+    <li><a href="in.linkedin.com/in/memonsaad/">Connect to Developer</a></li>
   </ul>
 </div>
   <br /><br />
