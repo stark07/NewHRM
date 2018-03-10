e34640716f402cad57c59b7b9feb2c267f0fdcaa
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-10 22:01:37 +0000

    Create search.php

---
+++ site/search.php
@@ -0,0 +1,6 @@
+$html = '<tr>';
+$html .= '<td class="small">nameString</td>';
+$html .= '<td class="small">compString</td>';
+$html .= '<td class="small">zipString</td>';
+$html .= '<td class="small">cityString</td>';
+$html .= '</tr>';
