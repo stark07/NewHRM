f76610231f6c8a342e39aed03462ddf75372aa66
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-10 23:03:15 +0000

    Update multientry.php

--- site/mulentry/multientry.php
+++ site/mulentry/multientry.php
@@ -6,54 +6,6 @@
     <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="//netsh.pp.ua/upwork-demo/1/js/typeahead.js"></script>
-  <style>
-        h1 {
-            font-size: 20px;
-            color: #111;
-        }
-
-        .content {
-            width: 80%;
-            margin: 0 auto;
-            margin-top: 50px;
-        }
-
-        .tt-hint,
-        .city {
-            border: 2px solid #CCCCCC;
-            border-radius: 8px 8px 8px 8px;
-            font-size: 24px;
-            height: 45px;
-            line-height: 30px;
-            outline: medium none;
-            padding: 8px 12px;
-            width: 400px;
-        }
-
-        .tt-dropdown-menu {
-            width: 400px;
-            margin-top: 5px;
-            padding: 8px 12px;
-            background-color: #fff;
-            border: 1px solid #ccc;
-            border: 1px solid rgba(0, 0, 0, 0.2);
-            border-radius: 8px 8px 8px 8px;
-            font-size: 18px;
-            color: #111;
-            background-color: #F1F1F1;
-        }
-    </style>  
-    
-  <script>
-        $(document).ready(function() {
-            $('input.name').typeahead({
-                name: 'name',
-                remote: 'mysql.php?query=%QUERY'
-            });
-        })
-    </script>
-    
-    
  </head>
  <body>
   <div class="content">
@@ -113,13 +65,6 @@
    </div>
    
   </div>
-  
-  
-  
-  
-  
-  
-  
  </body>
 </html>
 
