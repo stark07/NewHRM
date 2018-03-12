60f80e8eca4b566706937dfb4f4f54d22b5d0171
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-12 15:10:11 +0000

    Update database.php

--- site/database.php
+++ site/database.php
@@ -13,23 +13,6 @@ include '/templates/header.php';
     <script src="//netsh.pp.ua/upwork-demo/1/js/typeahead.js"></script>
  </head>
  <body>
-    <div class="btn-group">
-  <button type="button" class="btn btn-danger">Menu</button>
-  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    <span class="caret"></span>
-    <span class="sr-only">Toggle Dropdown</span>
-  </button>
-  <ul class="dropdown-menu">
-    <li><a href="../index.php">Home</a></li>
-    <li><a href="../profile.php">Profile</a></li>
-    <li><a href="../logout.php">Logout</a></li>
-    <li role="separator" class="divider"></li>
-    <li><a href="in.linkedin.com/in/memonsaad/">Connect to Developer</a></li>
-  </ul>
-</div>
- <br /><br />
-  <div class="container">
-   <br />
 
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
