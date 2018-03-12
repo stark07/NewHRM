d2cfb731edd7ad704cbff8378e40d3936072bcc7
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-12 16:18:17 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,12 @@ include 'templates/header.php';
         
             <!-- Left nav
             ================================================== -->
-<?php
-include 'site/database.php';
-?>
-            <div class="row">
-              <div class="span3 bs-docs-sidebar">
-                <ul class="nav nav-list bs-docs-sidenav">
 
                   <?php if($user->isAdmin()): ?>
                   
                   
                   <?php endif;
                         ?>
-                </ul>
-              </div>
-              </div>
-            </div>
-
     <script src="ASLibrary/js/asengine.js" type="text/javascript" charset="utf-8"></script>
     <script type="text/javascript" src="ASLibrary/js/index.js" charset="utf-8"></script>
   </body>
