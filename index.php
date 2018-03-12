1740588cc2a7ac9f6e4eee3fa7f5f0364f2eaa1e
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-12 15:02:37 +0000

    Update index.php

--- index.php
+++ index.php
@@ -53,10 +53,11 @@ include 'templates/header.php';
               </div>
             </div>
         
-    <?php include 'templates/footer.php';
-     ?>
+   
 
     <script src="ASLibrary/js/asengine.js" type="text/javascript" charset="utf-8"></script>
     <script type="text/javascript" src="ASLibrary/js/index.js" charset="utf-8"></script>
   </body>
 </html>
+ <?php include 'templates/footer.php';
+     ?>
