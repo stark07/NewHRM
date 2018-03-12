74550d046b8d846dbbf3e1d7b4894fb9b919df37
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-12 15:59:18 +0000

    Add files via upload

---
+++ site/index.php
@@ -0,0 +1,14 @@
+<?php
+include 'templates/header.php';
+include 'site/database.php.php';
+?>
+        
+           
+        
+    <?php include 'templates/footer.php';
+     ?>
+
+    <script src="ASLibrary/js/asengine.js" type="text/javascript" charset="utf-8"></script>
+    <script type="text/javascript" src="ASLibrary/js/index.js" charset="utf-8"></script>
+  </body>
+</html>
