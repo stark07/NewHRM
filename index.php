b87037192485c6c9340dd5d157f780061bdcb28d
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-12 16:09:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ include 'templates/header.php';
         
             <!-- Left nav
             ================================================== -->
+<?php
+include 'site/database.php';
+?>
             <div class="row">
               <div class="span3 bs-docs-sidebar">
                 <ul class="nav nav-list bs-docs-sidenav">
