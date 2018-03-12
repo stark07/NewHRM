1d01463d3385d097d0bfccc5ba3a0faf4e295c8d
stark07 <36279410+stark07@users.noreply.github.com>
2018-03-12 15:12:43 +0000

    Update database.php

--- site/database.php
+++ site/database.php
@@ -3,7 +3,7 @@
  <head>
     <meta charset="UTF-8">
     <title>Dashboard </title>
-    <<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
     <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="//netsh.pp.ua/upwork-demo/1/js/typeahead.js"></script>
